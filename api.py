fab7368fb1fbff57cda56b339c7b83a134f8c338
Sakshi Galgale <Sakshigalgale29@gmail.com>
2026-08-17 11:53:00 +0100

    Allow deployed frontend CORS

--- api.py
+++ api.py
@@ -24,7 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://orbit-ai-agent.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
